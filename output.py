e2b0420127c1c642c6411d31b0ff29cebf291077
jflannpsu26 <168307778+jflannpsu26@users.noreply.github.com>
2024-04-27 18:49:38 -0400

    Add files via upload

---
+++ output.py
@@ -0,0 +1,6 @@
+import DATABASE #imports the database file so it can be used for the printy line
+
+print(DATABASE.sentence6[1], DATABASE.sentence4[54], DATABASE.sentence5[0], DATABASE.sentence3[2], DATABASE.sentence4[7], DATABASE.sentence4[25], DATABASE.sentence2[3], DATABASE.sentence1[6], DATABASE.sentence3[6], ".", DATABASE.sentence4[3], DATABASE.sentence7[0][1:3], DATABASE.sentence5[3], DATABASE.sentence5[4], DATABASE.sentence4[7], DATABASE.sentence7[1:3], 
+      "'s", DATABASE.sentence4[31], DATABASE.sentence4[22][0:6], DATABASE.sentence5[11:14]) #takes words and letters out from all of the sentences that were in the original paragraph through string indexing and slicing.
+
+#output = Harry put the photo of Dudley on the mantel.  There is no sign of aunt Petunia’s baby bonnet in the house.
